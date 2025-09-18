84c8e398826c26a900ee3cb727116cded3ae06b9
Julien FRANCAIS <jfrancai@student.42.fr>
2025-09-18 15:48:53 +0200

    fix: wip

--- packages/v0-computation/src/Simulation/index.ts
+++
@@ -1,2 +0,0 @@
-export { simulate } from "./simulation";
-
--- packages/v0-computation/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./simulation/"
-export { computeAPR } from "./APR";
-export { computeTotalAssetsAtHighWaterMark } from "./totalAssetsAtHighWaterMark";
-export { getLastPeriodSummaryInDuration } from "./periodSummaries";
-export * from "./types"
