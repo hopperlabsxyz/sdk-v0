8546865f458a4481c1181b15e7375807523c4945
Julien FRANCAIS <jfrancai@student.42.fr>
2026-01-02 18:51:24 +0100

    fix: events

--- packages/v0-core/src/events/vault/TotalAssetsUpdated.ts
+++ packages/v0-core/src/events/vault/TotalAssetsUpdated.ts
@@ -3,14 +3,17 @@ import { Log, type ILog } from "../Log";
 
 interface ITotalAssetsUpdated extends ILog {
   totalAssets: BigIntish;
+  totalSupply?: BigIntish | null;
 }
 
 export class TotalAssetsUpdated extends Log {
   public readonly name: 'TotalAssetsUpdated' = 'TotalAssetsUpdated';
   public readonly totalAssets: bigint;
+  public readonly totalSupply: bigint | null;
 
-  constructor({ totalAssets, ...args }: ITotalAssetsUpdated) {
+  constructor({ totalAssets, totalSupply, ...args }: ITotalAssetsUpdated) {
     super(args);
     this.totalAssets = BigInt(totalAssets);
+    this.totalSupply = totalSupply ? BigInt(totalSupply) : null;
   }
 }
